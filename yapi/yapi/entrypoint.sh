80fde17bae4028702cdef68f64ef3d882661dc96
liangguohun <liangguohun>
2019-10-14 19:42:52 +0800

    首次运行镜像要安装依赖，其他情况不用

--- yapi/yapi/entrypoint.sh
+++ yapi/yapi/entrypoint.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-#yapi 初始化后会有一个init.lock文件
+#yapi 初始化后会有一个init.lock文件 根据获取的环境变量决定，默认false不初始化
 lockPath="/yapi/init.lock"
 
 # 设置源为淘宝源
@@ -10,12 +10,18 @@ npm config set registry http://registry.npm.taobao.org/;
 cd /yapi/vendors
 
 
+
 # 如果初始化文件文件存在,则直接运行,否则初始化
 if [ ! -f "$lockPath" ]; then
   # 全局安装用来更新yapi的cli
   npm i -g node-gyp yapi-cli;
   # 安装初始化的依赖模块
   npm i --production;
+  
+  node server/generlock.js
+fi
+
+if [ "$1" = true ] && [ ! -f "$lockPath" ]; then
   # 启动Yapi初始化
   node server/install.js
 else

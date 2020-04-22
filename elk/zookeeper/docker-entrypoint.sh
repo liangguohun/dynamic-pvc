cdb42d5caaec1743e8abf3b247d02e2a2db34dbc
liangguohun <liangguohun>
2020-04-22 11:02:31 +0800

    zk 镜像容器再k8s 的部署调整

--- elk/zookeeper/docker-entrypoint.sh
+++ elk/zookeeper/docker-entrypoint.sh
@@ -4,40 +4,44 @@ set -e
 
 # Allow the container to be started with `--user`
 if [[ "$1" = 'zkServer.sh' && "$(id -u)" = '0' ]]; then
-    chown -R zookeeper "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR" "$ZOO_LOG_DIR" "$ZOO_CONF_DIR"
+    # chown -R zookeeper "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR" "$ZOO_LOG_DIR" "$ZOO_CONF_DIR"
+    chown -R zookeeper "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR" "$ZOO_LOG_DIR"
     exec gosu zookeeper "$0" "$@"
 fi
 
 # Generate the config only if it doesn't exist
-if [[ ! -f "$ZOO_CONF_DIR/zoo.cfg" ]]; then
-    CONFIG="$ZOO_CONF_DIR/zoo.cfg"
-    {
-        echo "dataDir=$ZOO_DATA_DIR" 
-        echo "dataLogDir=$ZOO_DATA_LOG_DIR"
+# 我们使用configmap管理所以不需要
 
-        echo "tickTime=$ZOO_TICK_TIME"
-        echo "initLimit=$ZOO_INIT_LIMIT"
-        echo "syncLimit=$ZOO_SYNC_LIMIT"
+# if [[ ! -f "$ZOO_CONF_DIR/zoo.cfg" ]]; then
+#     CONFIG="$ZOO_CONF_DIR/zoo.cfg"
+#     {
+#         echo "dataDir=$ZOO_DATA_DIR" 
+#         echo "dataLogDir=$ZOO_DATA_LOG_DIR"
 
-        echo "autopurge.snapRetainCount=$ZOO_AUTOPURGE_SNAPRETAINCOUNT"
-        echo "autopurge.purgeInterval=$ZOO_AUTOPURGE_PURGEINTERVAL"
-        echo "maxClientCnxns=$ZOO_MAX_CLIENT_CNXNS"
-        echo "standaloneEnabled=$ZOO_STANDALONE_ENABLED"
-        echo "admin.enableServer=$ZOO_ADMINSERVER_ENABLED"
-    } >> "$CONFIG"
-    if [[ -z $ZOO_SERVERS ]]; then
-      ZOO_SERVERS="server.1=localhost:2888:3888;2181"
-    fi
+#         echo "tickTime=$ZOO_TICK_TIME"
+#         echo "initLimit=$ZOO_INIT_LIMIT"
+#         echo "syncLimit=$ZOO_SYNC_LIMIT"
 
-    for server in $ZOO_SERVERS; do
-        echo "$server" >> "$CONFIG"
-    done
+#         echo "autopurge.snapRetainCount=$ZOO_AUTOPURGE_SNAPRETAINCOUNT"
+#         echo "autopurge.purgeInterval=$ZOO_AUTOPURGE_PURGEINTERVAL"
+#         echo "maxClientCnxns=$ZOO_MAX_CLIENT_CNXNS"
+#         echo "standaloneEnabled=$ZOO_STANDALONE_ENABLED"
+#         echo "admin.enableServer=$ZOO_ADMINSERVER_ENABLED"
+#     } >> "$CONFIG"
+    
+#     if [[ -z $ZOO_SERVERS ]]; then
+#       ZOO_SERVERS="server.1=localhost:2888:3888;2181"
+#     fi
 
-    if [[ -n $ZOO_4LW_COMMANDS_WHITELIST ]]; then
-        echo "4lw.commands.whitelist=$ZOO_4LW_COMMANDS_WHITELIST" >> "$CONFIG"
-    fi
+#     for server in $ZOO_SERVERS; do
+#         echo "$server" >> "$CONFIG"
+#     done
 
-fi
+#     if [[ -n $ZOO_4LW_COMMANDS_WHITELIST ]]; then
+#         echo "4lw.commands.whitelist=$ZOO_4LW_COMMANDS_WHITELIST" >> "$CONFIG"
+#     fi
+
+# fi
 
 # Write myid only if it doesn't exist
 if [[ ! -f "$ZOO_DATA_DIR/myid" ]]; then
